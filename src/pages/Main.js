1a33fbef46a362bb36102d60c634180d08a2e72f
seoyeon5117 <seoyun020207@gmail.com>
2024-04-11 21:48:37 +0900

    feat: search 컴포넌트 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,14 +3,12 @@ import React from 'react';
 import './Main.css';
 import Header from '../components/Header.js';
 import Map from '../components/Map.js';
-import Search from '../components/Search.js';
 import Footer from '../components/Footer.js';
 
 export default function Main() {
     return (
         <>
             <header className="mainHeader"><Header /></header>
-            <Search />
             <Footer />
         </>
     )
