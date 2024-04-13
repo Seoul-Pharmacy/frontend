696a785627c99fe2bfe5675bd8fc1b3e6b56cbd5
seoyeon5117 <seoyun020207@gmail.com>
2024-04-13 14:04:51 +0900

    feat: 약국 찾기(내 위치 근처로 찾기, 지역으로 찾기 선택) 버튼

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import './Main.css';
 import Header from '../components/Header.js';
@@ -6,9 +7,26 @@ import Map from '../components/Map.js';
 import Footer from '../components/Footer.js';
 
 export default function Main() {
+    const { t } = useTranslation();
     return (
         <>
             <header className="mainHeader"><Header /></header>
+            <main className="selection">
+                <section>
+                    <p>관련 로고</p>
+                    <button>
+                        <p>{t('description.find_near')}</p>
+                        <p>{t('description.find_near_description')}</p>
+                    </button>
+                </section>
+                <section>
+                    <p>관련 로고</p>
+                    <button>
+                        <p>{t('description.find_by_region')}</p>
+                        <p>{t('description.find_by_region_description')}</p>
+                    </button>
+                </section>
+            </main>
             <Footer />
         </>
     )
