80d75305163ba0cef88d56d0e1175bfd40f049bf
seoyeon5117 <seoyun020207@gmail.com>
2024-04-27 21:54:23 +0900

    feat: intro image 위에 intro 추가

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
+import ko from '../images/KoImage.png';
+import en from '../images/EnImage.png';
+import cn from '../images/CnImage.png';
+import jp from '../images/JpImage.png';
+import Footer from '../components/Footer';
 import './Intro.css';
 
 const lngs = {
@@ -16,19 +21,21 @@ export default function Intro() {
     const { i18n } = useTranslation();
     return (
         <div className="Intro-body">
-            <h1>What is Seoul Pharmacy?</h1>
-            <p className="intro">This is a website looking for late-night pharmacies and foreign-speaking pharmacies around me in Korea.</p>
-            <h2>Select Language</h2>
-            <div>
+            <div className="intro">
+                <div className="intro-text">
+                    <h1>What is PharmaSeoul?</h1>
+                    <p>Guide of the location of nearby pharmacies that are currently open.</p>
+                </div>
+            </div>
+            
+            <div className="language-body">
+                <h2>Select a language.</h2>
                 {Object.keys(lngs).map((lng) => (
-                    <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {
-                        i18n.changeLanguage(lng);
-                        navigate('/main');
-                    }}>
-                        {lngs[lng].nativeName}
+                    <button className="language" key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit">
                     </button>
                 ))}
             </div>
+            <Footer />
         </div>
     )
 }
