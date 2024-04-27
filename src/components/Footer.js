7cb7657fba9f73ace2790d42eceab37e3f6b1ed3
seoyeon5117 <seoyun020207@gmail.com>
2024-04-27 22:14:43 +0900

    feat: footer 보이게 css 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 import { useTranslation } from 'react-i18next';
+import './Footer.css';
 
 export default function Footer() {
   const { t } = useTranslation();
 
   return (
-    <div className="App">
+    <>
       <footer>
         <p>{t('description.GEN')}</p>
         <p>{t('description.GroupName')}</p>
         <p>{t('description.Insta')}</p>
         <p>{t('description.Email')}</p>
       </footer>
-    </div>
+    </>
   );
 }
