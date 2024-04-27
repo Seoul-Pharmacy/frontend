e95d2197d701a756c1a20606ad0387bb90024aba
seoyeon5117 <seoyun020207@gmail.com>
2024-04-28 04:57:49 +0900

    feat: main 화면에 옵션 버튼 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,30 +3,45 @@ import { useTranslation } from 'react-i18next';
 
 import './Main.css';
 import Header from '../components/Header.js';
-import Map from '../components/Map.js';
+import Nearby from '../images/nearby.png';
+import Region from '../images/region.png';
+import Mark from '../images/locationMarkIcon.png';
 import Footer from '../components/Footer.js';
 
 export default function Main() {
     const { t } = useTranslation();
     return (
         <>
+            <div className="main-wrapper">
             <header className="mainHeader"><Header /></header>
             <main className="selection">
+                <div>
+                    <img className="locationMark" src={Mark} alt="location" />
+                </div>
                 <section>
-                    <p>관련 로고</p>
-                    <button>
-                        <p>{t('description.find_near')}</p>
-                        <p>{t('description.find_near_description')}</p>
+                    <button
+                        className="selectOption"
+                        type="submit"                      style={{
+                            backgroundImage: `url(${Nearby})`
+                        }}>
+                        <div>
+                            <p>{t('description.find_near')}</p>
+                            <p>{t('description.find_near_description')}</p>
+                        </div>
                     </button>
                 </section>
                 <section>
-                    <p>관련 로고</p>
-                    <button>
+                <button
+                        className="selectOption"
+                        type="submit"                      style={{
+                            backgroundImage: `url(${Region})`
+                        }}>
                         <p>{t('description.find_by_region')}</p>
                         <p>{t('description.find_by_region_description')}</p>
                     </button>
                 </section>
             </main>
+            </div>
             <Footer />
         </>
     )
