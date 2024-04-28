935482538eb0c0a6349766e0564e97ee7c12e41b
hl3470 <hl3470@naver.com>
2024-04-28 22:21:35 +0900

    feat: footer 정보 추가
    대표자, 팀명, 전화번호, 인스타 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,25 @@
 import { useTranslation } from 'react-i18next';
 import './Footer.css';
 
+const FOOTER_TEXT = {
+    "owner" : "윤찬호",
+    "GEN": "010-4158-7384",
+    "groupName": "어디약? 여기약!",
+    "insta": "https://www.instagram.com/pharmaseoul__/",
+    "email": "",
+}
+
 export default function Footer() {
   const { t } = useTranslation();
 
   return (
     <>
-      <footer>
-        <p>{t('description.GEN')}</p>
-        <p>{t('description.GroupName')}</p>
-        <p>{t('description.Insta')}</p>
-        <p>{t('description.Email')}</p>
-      </footer>
+        <   footer>
+            <p>{t('description.owner')} <span>{FOOTER_TEXT.owner}</span></p>
+            <p>{t('description.groupName')} <span>{FOOTER_TEXT.groupName}</span></p>
+            <p>{t('description.GEN')} <span>{FOOTER_TEXT.GEN}</span></p>
+            <p>{t('description.email')} <span>{FOOTER_TEXT.email}</span></p>
+        </footer>
     </>
   );
 }
