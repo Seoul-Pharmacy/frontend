1d454f1abc34b1446505be8264dfcad59a170a14
seoyeon5117 <seoyun020207@gmail.com>
2024-04-29 14:58:30 +0900

    feat: 지역 선택으로 찾기 버튼 클릭 시 팝업

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import Modal from 'react-modal';
 
 import './Main.css';
 import Header from '../components/Header.js';
@@ -10,17 +11,38 @@ import Glasses from '../images/readingGlassesIcon.png';
 import Mark from '../images/locationMarkIcon.png';
 import Footer from '../components/Footer.js';
 
+// 모달 컴포넌트 정의
+function PopUp({ isOpen, toggleModal }) {
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={toggleModal}
+            ariaHideApp={false}
+            contentLabel="Region Selection Modal"
+            shouldCloseOnOverlayClick={false}
+        >
+            <h2>지역 선택</h2>
+            <button onClick={toggleModal}>닫기</button>
+        </Modal>
+    );
+}
+
+// 메인 컴포넌트 정의
 export default function Main() {
     const navigate = useNavigate();
     const { t } = useTranslation();
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const toggleModal = () => setModalOpen(!modalOpen);
+
     return (
         <>
             <div className="main-wrapper">
-                <header className="mainHeader"><Header /></header>
+                <Header />
                 <main className="selection">
                     <div>
-                        <img src={Glasses} alt="reading glasses"/>
-                        <img className="locationMark" src={Mark} alt="location" />
+                        <img src={Glasses} alt="reading glasses" />
+                        <img className="locationMark" src={Mark} alt="location mark" />
                     </div>
                     <section>
                         <div
@@ -29,7 +51,9 @@ export default function Main() {
                             }}>
                             <p className="selectOptionTitle">{t('description.find_near')}</p>
                             <p className="selectOptionDescription">{t('description.find_near_description')}</p>
-                            <button className="whiteSelectButton" onClick={() => navigate('/nearbysearch')}>선택하기</button>
+                            <button className="whiteSelectButton" onClick={() => navigate('/nearbysearch')}>
+                                선택하기
+                            </button>
                         </div>
                         <div
                             className="selectOptionButton" style={{
@@ -37,12 +61,15 @@ export default function Main() {
                             }}>
                             <p className="selectOptionTitle">{t('description.find_by_region')}</p>
                             <p className="selectOptionDescription">{t('description.find_by_region_description')}</p>
-                            <button className="whiteSelectButton" onClick={() => navigate('/regionsearch')}>선택하기</button>
+                            <button className="whiteSelectButton" onClick={toggleModal}>
+                                선택하기
+                            </button>
                         </div>
                     </section>
                 </main>
+                <Footer />
             </div>
-            <Footer />
+            <PopUp isOpen={modalOpen} toggleModal={toggleModal} />
         </>
-    )
+    );
 }
