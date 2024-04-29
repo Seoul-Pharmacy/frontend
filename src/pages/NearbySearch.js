d09dacb06b6c240773fd2fe1ade3fe150e9559da
seoyeon5117 <seoyun020207@gmail.com>
2024-04-29 17:20:32 +0900

    feat: nearbySearch 초기 작업(디자인)

--- src/pages/NearbySearch.js
+++ src/pages/NearbySearch.js
@@ -1,3 +1,24 @@
-export default function NearbySearch() {
+import React from "react";
+import './NearbySearch.css';
 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import LeftRectangle from '../images/NearbySearchPage/leftRectangle.png';
+import RightRectangle from '../images/NearbySearchPage/rightRectangle.png';
+import GreenBag from '../images/NearbySearchPage/greenBag.png';
+import Hand from '../images/NearbySearchPage/hand.png';
+
+export default function NearbySearch() {
+    return(
+        <>
+            <Header />
+            <div className="nearbySearchDesign">
+                <img className="leftRec" src={LeftRectangle} />
+                <img className="rightRec" src={RightRectangle} />
+                <img className="greenBag" src={GreenBag} />
+                <img className="hand" src={Hand} />
+            </div>
+            <Footer />
+        </>
+    )
 }
