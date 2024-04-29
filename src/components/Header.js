cbf0089ff66aa04af581a1e4478aa3807b921e9c
seoyeon5117 <seoyun020207@gmail.com>
2024-04-29 18:17:21 +0900

    refactor: 헤더의 언어 선택 버튼을 드롭다운으로 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,30 +4,33 @@ import homeIcon from '../images/homeIcon.png';
 
 const lngs = {
   en: { nativeName: 'English' },
-  ko: { nativeName: "Korean" },
-  cn: { nativeName: 'Chinese' },
-  jp: { nativeName: 'Japanese' },
+  ko: { nativeName: '한국어' },
+  cn: { nativeName: '华侨华人' },
+  jp: { nativeName: '日本語です' },
 };
 
 export default function Header() {
   const { i18n } = useTranslation();
 
+  const handleLanguageChange = (event) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
   return (
     <>
       <header>
-        <div>
+        <div className="header-content">
           <a href="/main">
             <img className="home-icon" src={homeIcon} alt="home"/>
           </a>
-          {Object.keys(lngs).map((lng) => (
-            <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
-              {lngs[lng].nativeName}
-            </button>
-          ))}
-          <a className="title" href="/main">pharamaseoul</a>
+          <select onChange={handleLanguageChange} className="language-select" value={i18n.resolvedLanguage}>
+            {Object.keys(lngs).map((lng) => (
+              <option key={lng} value={lng}>{lngs[lng].nativeName}</option>
+            ))}
+          </select>
+          <a className="title" href="/main">PharmaSeoul</a>
         </div>
       </header>
     </>
   );
 }
-
