df59ca9432fa0211fc3fa95881e0f4848e584f01
hl3470 <hl3470@naver.com>
2024-04-30 14:28:36 +0900

    feat: intro 페이지 css 구현

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -10,10 +10,10 @@ import Footer from '../components/Footer';
 import './Intro.css';
 
 const lngs = {
-    ko: { nativeName: 'Korean', imageName: koImg},
-    en: { nativeName: 'English', imageName: enImg},
-    cn: { nativeName: 'Chinese', imageName: cnImg},
-    jp: { nativeName: 'Japanese', imageName: jpImg},
+    ko: { nativeName: '한국어', imageName: koImg },
+    en: { nativeName: 'English', imageName: enImg },
+    cn: { nativeName: '华侨华人', imageName: cnImg },
+    jp: { nativeName: '日本語です', imageName: jpImg },
 };
 
 export default function Intro() {
@@ -27,25 +27,26 @@ export default function Intro() {
                     <p>Guide of the location of nearby pharmacies that are currently open.</p>
                 </div>
             </div>
-            
             <div className="language-body">
-                <h2>Select a language.</h2>
-                {Object.keys(lngs).map((lng) => (
-                    <button
-                        className="intro-lan-button" key={lng}
-                        type="submit"
-                        style={{
-                            backgroundImage: `url(${lngs[lng].imageName})`
-                        }}
-                        onClick={() => {
-                            i18n.changeLanguage(lng);
-                            navigate('/main');
-                        }}>
-                        {lngs[lng].nativeName}
-                    </button>
-                ))}
+                <div className="language-body-inner">
+                    <div className="language-body-name">Select a language.</div>
+                    {Object.keys(lngs).map((lng) => (
+                        <button
+                            className="intro-lan-button" key={lng}
+                            type="submit"
+                            style={{
+                                backgroundImage: `url(${lngs[lng].imageName})`
+                            }}
+                            onClick={() => {
+                                i18n.changeLanguage(lng);
+                                navigate('/main');
+                            }}>
+                            {lngs[lng].nativeName}
+                        </button>
+                    ))}
+                </div>
             </div>
-            <Footer />
+            <Footer/>
         </div>
     )
 }
