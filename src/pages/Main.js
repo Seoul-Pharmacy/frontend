96d85dbd5787b15b7bd10c18d5eb62e79dccd18a
seoyeon5117 <seoyun020207@gmail.com>
2024-05-03 00:12:16 +0900

    refactor: modal 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,47 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import Modal from 'react-modal';
-
 import useUserLocation from '../Hooks/useUserLocation.js';
+
 import './Main.css';
+
 import Header from '../components/Header.js';
+import Footer from '../components/Footer.js';
 
 import Nearby from '../images/nearby.png';
 import Region from '../images/region.png';
 import Glasses from '../images/readingGlassesIcon.png';
 import Mark from '../images/locationMarkIcon.png';
 
-import CloseButton from '../images/close.png';
-
-import Footer from '../components/Footer.js';
-
-function PopUp({ isOpen, toggleModal }) {
-    const [selectedOption, setSelectedOption] = useState('');
-
-    const handleDropdownChange = (event) => {
-        setSelectedOption(event.target.value);
-    };
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={toggleModal}
-            ariaHideApp={false}
-            contentLabel="Region Selection Modal"
-            shouldCloseOnOverlayClick={false}
-            overlayClassName="regionModal-overlay"
-            className="regionModal-content"
-        >
-            <img className="closeButton" src={CloseButton} onClick={toggleModal} alt="close button" />
-            <h2>지역과 시간을 설정해보세요!</h2>
-            <p>영업시간</p>
-            <select value={selectedOption} onChange={handleDropdownChange}>
-                <option></option>
-            </select>
-            <button className="selectionComplete">선택완료</button>
-        </Modal>
-    );
-}
 
 export default function Main() {
     const navigate = useNavigate();
@@ -55,10 +26,6 @@ export default function Main() {
             console.error("Location data is not available.");
         }
     };
-
-    const [modalOpen, setModalOpen] = useState(false);
-
-    const toggleModal = () => setModalOpen(!modalOpen);
 
     return (
         <>
@@ -89,7 +56,7 @@ export default function Main() {
                             }}>
                             <h3 className="selectOptionTitle">{t('description.find_by_region')}</h3>
                             <p className="selectOptionDescription">{t('description.find_by_region_description')}</p>
-                            <button className="SelectOptionInnerButton" onClick={toggleModal}>
+                            <button className="SelectOptionInnerButton">
                                 {t('description.select')}
                             </button>
                         </div>
@@ -97,7 +64,6 @@ export default function Main() {
                 </div>
             </div>
             <Footer />
-            <PopUp isOpen={modalOpen} toggleModal={toggleModal} />
         </>
     );
 }
