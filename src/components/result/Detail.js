48836a0bb10207ff2420369243a096054e100c89
seoyeon5117 <seoyun020207@gmail.com>
2024-05-05 14:03:14 +0900

    design: 검색결과 detail css 추가

--- src/components/result/Detail.js
+++ src/components/result/Detail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import apiDetail from '../../api/apiDetail';
 import Map from "../map/Map";
+import './Detail.css';
 
 export default function Detail({ identifier }) {
     const [pharmacy, setPharmacy] = useState(null);
@@ -51,15 +52,17 @@ export default function Detail({ identifier }) {
             <Map lat={parseFloat(pharmacy.latitude)} lng={parseFloat(pharmacy.longitude)} />
             <div id="result-details-text-wrapper">
                 <h1 id="result-details-name">약국 이름 | {pharmacy.name}</h1>
-                <div id="result-details-name">주소 | {pharmacy.si} {pharmacy.gu} {pharmacy.road_name_address} </div>
-                <div id="result-details-name">영업시간 | 월요일: {pharmacy.mon_open_time}~{pharmacy.mon_close_time}
-                <p>화요일: {pharmacy.tue_open_time}~{pharmacy.tue_close_time}</p>
-                <p>수요일: {pharmacy.wed_open_time}~{pharmacy.wed_close_time}</p></div>
-                <p>목요일: {pharmacy.thu_open_time}~{pharmacy.thu_close_time}</p>
-                <p>금요일: {pharmacy.fri_open_time}~{pharmacy.fri_close_time}</p>
-                <p>토요일: {pharmacy.sat_open_time}~{pharmacy.sat_close_time}</p>
-                <p>일요일: {pharmacy.sun_open_time}~{pharmacy.sun_close_time}</p>
-                <p>공휴일: {pharmacy.holiday_open_time}~{pharmacy.holiday_close_time}</p>
+                <div id="result-details-address">주소 | {pharmacy.si} {pharmacy.gu} {pharmacy.road_name_address}</div>
+                <div id="result-details-time">
+                    <p>영업시간 | 월요일: {pharmacy.mon_open_time}~{pharmacy.mon_close_time}</p>
+                    <p>화요일: {pharmacy.tue_open_time}~{pharmacy.tue_close_time}</p>
+                    <p>수요일: {pharmacy.wed_open_time}~{pharmacy.wed_close_time}</p>
+                    <p>목요일: {pharmacy.thu_open_time}~{pharmacy.thu_close_time}</p>
+                    <p>금요일: {pharmacy.fri_open_time}~{pharmacy.fri_close_time}</p>
+                    <p>토요일: {pharmacy.sat_open_time}~{pharmacy.sat_close_time}</p>
+                    <p>일요일: {pharmacy.sun_open_time}~{pharmacy.sun_close_time}</p>
+                    <p>공휴일: {pharmacy.holiday_open_time}~{pharmacy.holiday_close_time}</p>
+                </div>
                 <div id="result-details-GEN">전화번호 | {pharmacy.main_number}</div>
             </div>
         </article>
