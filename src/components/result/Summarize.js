272d52da87766fd16da5ee872080b5cc0b7ae785
seoyeon5117 <seoyun020207@gmail.com>
2024-05-05 13:44:45 +0900

    design: summarize css

--- src/components/result/Summarize.js
+++ src/components/result/Summarize.js
@@ -1,18 +1,24 @@
+import './Summarize.css';
+
 export default function({ id, name, distance = 0, si, gu, road_name_address }) {
 
     if (id === 'nearby-summarize') {
         return(
-            <div>
-                <h6>{name}</h6>
-                <p>내 위치에서 {distance}</p>
-                <p>{si} {gu} {road_name_address}</p>
+            <div id="summarize-result">
+                <h6 id="summarize-name">{name}</h6>
+                <p id="summarize-info">
+                    <div>내 위치에서 {distance}</div>
+                    <div>{si} {gu} {road_name_address}</div>
+                </p>
             </div>
         );
     } else {
         return(
-            <div>
-                <h6>{name}</h6>
-                <p>{si} {gu} {road_name_address}</p>
+            <div id="summarize-result">
+                <h6 id="summarize-name">{name}</h6>
+                <p id="summarize-info">
+                    <div>{si} {gu} {road_name_address}</div>
+                </p>
             </div>
         );
     }
