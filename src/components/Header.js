301c0c6a585024f8cc240d25a3e667d1c8a3f03c
hl3470 <hl3470@naver.com>
2024-05-07 15:29:26 +0900

    fix: header의 언어 드롭다운 오류 수정
    드롭다운 hover와 click이벤트가 중첩되어 이상하게 움직이는 문제 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import './Header.css';
 import homeIcon from '../images/homeIcon.png';
+import {Dropdown} from "react-bootstrap";
 
 const lngs = {
     en: {nativeName: 'English'},
@@ -12,37 +13,51 @@ const lngs = {
 
 export default function Header() {
     const {i18n} = useTranslation();
-    const [dropdownOpen, setDropdownOpen] = useState(false);
+    // const [dropdownOpen, setDropdownOpen] = useState(false);
     const [selectedLang, setSelectedLang] = useState(i18n.resolvedLanguage);
 
-    const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+    // const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
     const handleSelectLang = (lang) => {
         i18n.changeLanguage(lang);
         setSelectedLang(lang);
-        setDropdownOpen(false);
+        // setDropdownOpen(false);
     };
 
     return (
         <>
             <header>
-                <div className="header-content">
+                <div id="header-content">
                     <a href="/main">
                         <img className="home-icon" src={homeIcon} alt="home"/>
                     </a>
-                    <div className="language-dropdown">
-                        <div className="dropdown-btn" onClick={toggleDropdown}>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="language-dropdown">
                             {lngs[selectedLang].nativeName}
-                        </div>
-                        {dropdownOpen && (
-                            <div className="dropdown-item-wrapper">
-                                {Object.keys(lngs).map((lng) => (
-                                    <div key={lng} className="dropdown-item" onClick={() => handleSelectLang(lng)}>
-                                        {lngs[lng].nativeName}
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-                    </div>
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            {Object.keys(lngs).map((lng) => (
+                                <Dropdown.Item key={lng} className="dropdown-item" onClick={() => handleSelectLang(lng)}>
+                                    {lngs[lng].nativeName}
+                                </Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    {/*<div className="language-dropdown">*/}
+                    {/*    <div className="dropdown-btn" onCli  ck={toggleDropdown}>*/}
+                    {/*        {lngs[selectedLang].nativeName}*/}
+                    {/*    </div>*/}
+                    {/*    {dropdownOpen && (*/}
+                    {/*        <div className="dropdown-item-wrapper">*/}
+                    {/*            {Object.keys(lngs).map((lng) => (*/}
+                    {/*                <div key={lng} className="dropdown-item" onClick={() => handleSelectLang(lng)}>*/}
+                    {/*                    {lngs[lng].nativeName}*/}
+                    {/*                </div>*/}
+                    {/*            ))}*/}
+                    {/*        </div>*/}
+                    {/*    )}*/}
+                    {/*</div>*/}
                     <a className="title" href="/main">pharmaseoul</a>
                 </div>
             </header>
