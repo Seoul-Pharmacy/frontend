421e749632c259bfe532b1ce46f1d5ad291ea92a
seoyeon5117 <seoyun020207@gmail.com>
2024-05-14 19:22:37 +0900

    fix: 오타 수정 및 공백 없는 문제 수정

--- src/components/result/Summarize.js
+++ src/components/result/Summarize.js
@@ -12,9 +12,9 @@ export default function Summarize({ id, name, distance = 0, si, gu, road_name_ad
                     <div>{t('from-me')}{distance}</div>
                     <div>{si} {gu} {road_name_address}</div>
                     <div>
-                        {speaking_english ? <span> english</span> : ''}
-                        {speaking_chinese ? <span> 中国人</span> : ''}
-                        {speaking_japanese ? <span> 日本語</span> : ''}
+                        {speaking_english ? <span>&nbsp;English</span> : ''}
+                        {speaking_chinese ? <span>&nbsp;中国語</span> : ''}
+                        {speaking_japanese ? <span>&nbsp;日本語</span> : ''}
                     </div>
                 </div>
             </div>
@@ -26,9 +26,9 @@ export default function Summarize({ id, name, distance = 0, si, gu, road_name_ad
                 <div id="summarize-info">
                     <div>{si} {gu} {road_name_address}</div>
                     <div>
-                        {speaking_english ? <span> english</span> : ''}
-                        {speaking_chinese ? <span> 中国人</span> : ''}
-                        {speaking_japanese ? <span>日本語</span> : ''}
+                        {speaking_english ? <span> English</span> : ''}
+                        {speaking_chinese ? <span>&nbsp;中国語</span> : ''}
+                        {speaking_japanese ? <span>&nbsp;日本語</span> : ''}
                     </div>
                 </div>
             </div>
